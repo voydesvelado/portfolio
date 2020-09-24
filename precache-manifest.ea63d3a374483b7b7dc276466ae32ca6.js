f380c581eb33ebea90225aeeacbae8d7f190aa10
novacane <mudakmudaka@gmail.com>
2020-09-23 20:04:31 -0500

    Updates

---
+++ precache-manifest.ea63d3a374483b7b7dc276466ae32ca6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bc737e50973f5d571a688998d35486",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "a26862980e0eee507e9e",
+    "url": "/curriculum/static/css/main.f948a415.chunk.css"
+  },
+  {
+    "revision": "246a525707184b534d18",
+    "url": "/curriculum/static/js/2.0e87fb42.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/curriculum/static/js/2.0e87fb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26862980e0eee507e9e",
+    "url": "/curriculum/static/js/main.bc288aa7.chunk.js"
+  },
+  {
+    "revision": "ea8537b673971a69b88d",
+    "url": "/curriculum/static/js/runtime-main.6f6a4d16.js"
+  },
+  {
+    "revision": "3339e7eb9be555747cec3b0804121c5a",
+    "url": "/curriculum/static/media/IMG_20200918_191251-02.3339e7eb.jpeg"
+  },
+  {
+    "revision": "f4d076539feb50e837bf14e3e449e169",
+    "url": "/curriculum/static/media/avatarb.f4d07653.jpeg"
+  },
+  {
+    "revision": "f8eb1fd95c91c54be9b6c4a66b26eaac",
+    "url": "/curriculum/static/media/lux.f8eb1fd9.jpg"
+  },
+  {
+    "revision": "747433645b89341ad89671502b712499",
+    "url": "/curriculum/static/media/random.74743364.png"
+  },
+  {
+    "revision": "46ca8ba18f0b7f1692b5ead9898d5051",
+    "url": "/curriculum/static/media/simplestore.46ca8ba1.png"
+  }
+]);
